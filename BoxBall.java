245eac693367e67d2e8bf5b185d7c387ce204228
Andrew Worthington <aworthington82@gmail.com>
2015-10-22 16:46:39 -0400

    scarp this

--- BoxBall.java
+++ BoxBall.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.geom.*;
 import java.awt.Color;
 import java.util.Random;
+import java.aw
 
 /**
  * Class BoxBall - a graphical ball that observes the effect of gravity. The ball
@@ -33,8 +34,7 @@ public class BoxBall
     private double hVelocity;
     private double friction = .04;
     private boolean onTheGround = false;
-    private BoxDims theBoxDims;
-
+    
     /**
      * Constructor for objects of class BouncingBall
      *
